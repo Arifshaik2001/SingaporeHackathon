3f3e5a233ba4a0bb81934d01951bdaa25ea0c1f6
BhureAbhi <cs15btech11001@iith.ac.in>
2018-11-12 18:07:11 +0530

    Click on marker to get user info.

--- web/liveTracking.js
+++ web/liveTracking.js
@@ -1,6 +1,6 @@
 
 function livetracking(map, firebase, markers){
-	var ref = firebase.database().ref("Locations");
+	var ref = firebase.database().ref("LiveLocations");
 	ref.on('value', getLocations)
 
 	function getLocations(data){
@@ -16,11 +16,24 @@ function livetracking(map, firebase, markers){
 			var k = keys[i];
 			var lat = tracks[k].lat;
 			var lon = tracks[k].lng;
+			var userid = tracks[k].uid;
+			var contact = tracks[k].email;
+
 			console.log(lat, lon);
-			var marker = new google.maps.Marker({position: new google.maps.LatLng(lat,lon)});
+			var marker = new google.maps.Marker({
+				position: new google.maps.LatLng(lat,lon),
+				userid: userid
+			});
 			marker.setIcon('http://maps.google.com/mapfiles/ms/icons/blue-dot.png')
 			marker.setMap(map);
 			markers.push(marker);
+
+			var infowindow = new google.maps.InfoWindow();
+			google.maps.event.addListener(marker, 'click', function(){
+				console.log("clicked");
+				infowindow.setContent('<div><strong>'+contact+'</strong></div>');
+				infowindow.open(map, this);
+			});
 		}
 
 	}
